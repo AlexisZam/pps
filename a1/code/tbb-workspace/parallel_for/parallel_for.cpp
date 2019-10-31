dee8c738b77efd92dfb72506215759e44a2649a3
Alexis Zamanis <alexiszamanis1997@gmail.com>
2019-10-31 15:16:07 +0200

    formatted

--- a1/code/tbb-workspace/parallel_for/parallel_for.cpp
+++ a1/code/tbb-workspace/parallel_for/parallel_for.cpp
@@ -20,8 +20,7 @@
  * Dummy work on a float
  *
  */
-void Foo(float &x)
-{
+void Foo(float &x) {
     x = x + 2.2;
     x = x / 3.3;
     x = x * 4.4;
@@ -34,39 +33,36 @@ void Foo(float &x)
  * Applies serially Foo on an float array
  * @param a float array
  * @param n size of array
- */  
-void SerialApplyFoo(float *a, size_t n) 
-{
-    for ( size_t i = 0; i != n; i++ )
+ */
+void SerialApplyFoo(float *a, size_t n) {
+    for (size_t i = 0; i != n; i++)
         Foo(a[i]);
 }
-
 
 /**
  * Class for function object that describes 
  * work to be performed on a blocked range 
- */ 
+ */
 class ApplyFoo {
-        float * const my_a;
+    float *const my_a;
 
-    public:
-        void operator()(const tbb::blocked_range<size_t>& r) const {
-            float *a = my_a;
+public:
+    void operator()(const tbb::blocked_range<size_t> &r) const {
+        float *a = my_a;
 
 #ifdef DEBUG
-            std::cerr << "Worker thread " << (pid_t)syscall(SYS_gettid)
-                      << ": [" << r.begin() 
-                      << "," << r.end() 
-                      << ")" << std::endl; 
+        std::cerr << "Worker thread " << (pid_t)syscall(SYS_gettid)
+                  << ": [" << r.begin()
+                  << "," << r.end()
+                  << ")" << std::endl;
 #endif
 
-            for ( size_t i = r.begin(); i != r.end(); i++ ) 
-                Foo(a[i]);
-            
-        }
+        for (size_t i = r.begin(); i != r.end(); i++)
+            Foo(a[i]);
+    }
 
-        // constructor
-        ApplyFoo(float *a) : my_a(a) {}
+    // constructor
+    ApplyFoo(float *a) : my_a(a) {}
 };
 
 /**
@@ -74,15 +70,14 @@ class ApplyFoo {
  * Uses functor inside parallel_for (the standard way)
  * @param a float array
  * @param n size of array
- */  
-void ParallelApplyFooStandard(float *a, size_t n)
-{
-    tbb::parallel_for( tbb::blocked_range<size_t>(0,n), 
-                        ApplyFoo(a),
-                        tbb::auto_partitioner()
-                    // or: tbb::simple_partitioner()
-                    // or: tbb::affinity_partitioner()
-                );
+ */
+void ParallelApplyFooStandard(float *a, size_t n) {
+    tbb::parallel_for(tbb::blocked_range<size_t>(0, n),
+                      ApplyFoo(a),
+                      tbb::auto_partitioner()
+                      // or: tbb::simple_partitioner()
+                      // or: tbb::affinity_partitioner()
+    );
 }
 
 #ifdef USE_LAMBDAS
@@ -92,49 +87,42 @@ void ParallelApplyFooStandard(float *a, size_t n)
  * Uses lambda expression (C++11 feature), compiles with gcc4.5 or newer.
  * @param a float array
  * @param n size of array
- */  
-void ParallelApplyFooLambda(float *a, size_t n)
-{
-    tbb::parallel_for( 
-            tbb::blocked_range<size_t>(0,n), 
-            [=](const tbb::blocked_range<size_t>& r) {
-                for ( size_t i = r.begin(); i != r.end(); ++i )
-                    Foo(a[i]);
-            } 
-            );
+ */
+void ParallelApplyFooLambda(float *a, size_t n) {
+    tbb::parallel_for(
+        tbb::blocked_range<size_t>(0, n),
+        [=](const tbb::blocked_range<size_t> &r) {
+            for (size_t i = r.begin(); i != r.end(); ++i)
+                Foo(a[i]);
+        });
 }
-
 
 /**
  * Executes in parallel Foo on a float array.
  * Uses nested parallelism, combining parallel_invoke and parallel_forlambda expression (C++11 feature), compiles with gcc4.5 or newer.
  * @param a float array
  * @param n size of array
- */  
-void ParallelApplyFooNested(float *a, size_t n)
-{
-    tbb::parallel_invoke( 
-            [=]() {
-            tbb::parallel_for( 
-                tbb::blocked_range<size_t>(0,n/2), 
-                [=](const tbb::blocked_range<size_t>& r) {
-                for ( size_t i = r.begin(); i != r.end(); ++i )
-                Foo(a[i]);
-                } 
-                );
-            }, 
+ */
+void ParallelApplyFooNested(float *a, size_t n) {
+    tbb::parallel_invoke(
+        [=]() {
+            tbb::parallel_for(
+                tbb::blocked_range<size_t>(0, n / 2),
+                [=](const tbb::blocked_range<size_t> &r) {
+                    for (size_t i = r.begin(); i != r.end(); ++i)
+                        Foo(a[i]);
+                });
+        },
 
-            [=]() {
-            tbb::parallel_for( 
-                tbb::blocked_range<size_t>(n/2,n), 
-                [=](const tbb::blocked_range<size_t>& r) {
-                for ( size_t i = r.begin(); i != r.end(); ++i )
-                Foo(a[i]);
-                } 
-                );
-            } );
+        [=]() {
+            tbb::parallel_for(
+                tbb::blocked_range<size_t>(n / 2, n),
+                [=](const tbb::blocked_range<size_t> &r) {
+                    for (size_t i = r.begin(); i != r.end(); ++i)
+                        Foo(a[i]);
+                });
+        });
 }
-
 
 #if 0
 /**
@@ -155,56 +143,57 @@ void ParallelApplyFooLambdaCompact(float *a, size_t n)
 
 #endif // !USE_LAMBDAS
 
-
-
 /**
  * @param argv[1] number of threads
  * @param argv[2] size of array
- */  
-int main(int argc, char **argv)
-{
+ */
+int main(int argc, char **argv) {
     const int nthreads = atoi(argv[1]);
     const int size = atoi(argv[2]);
 
-    tbb::task_scheduler_init init(nthreads); 
+    tbb::task_scheduler_init init(nthreads);
 
-    float *b = new float [size];
-    
-    for ( int i = 0; i < size; i++ ) b[i] = 0.0;
+    float *b = new float[size];
 
-    tbb::tick_count tic,toc; 
+    for (int i = 0; i < size; i++)
+        b[i] = 0.0;
+
+    tbb::tick_count tic, toc;
 
     tic = tbb::tick_count::now();
     SerialApplyFoo(b, size);
     toc = tbb::tick_count::now();
-    std::cout << "Serial : " << (toc-tic).seconds() 
+    std::cout << "Serial : " << (toc - tic).seconds()
               << " seconds" << std::endl;
 
-    for ( int i = 0; i < size; i++ ) b[i] = 0.0;
+    for (int i = 0; i < size; i++)
+        b[i] = 0.0;
     tic = tbb::tick_count::now();
     ParallelApplyFooStandard(b, size);
     toc = tbb::tick_count::now();
-    std::cout << "Parallel standard : " << (toc-tic).seconds() 
+    std::cout << "Parallel standard : " << (toc - tic).seconds()
               << " seconds" << std::endl;
 
 #ifdef USE_LAMBDAS
-    for ( int i = 0; i < size; i++ ) b[i] = 0.0;
+    for (int i = 0; i < size; i++)
+        b[i] = 0.0;
     tic = tbb::tick_count::now();
     ParallelApplyFooLambda(b, size);
     toc = tbb::tick_count::now();
-    std::cout << "Parallel lambda : " << (toc-tic).seconds() 
+    std::cout << "Parallel lambda : " << (toc - tic).seconds()
               << " seconds" << std::endl;
 
-    for ( int i = 0; i < size; i++ ) b[i] = 0.0;
+    for (int i = 0; i < size; i++)
+        b[i] = 0.0;
     tic = tbb::tick_count::now();
     ParallelApplyFooNested(b, size);
     toc = tbb::tick_count::now();
-    std::cout << "Parallel nested : " << (toc-tic).seconds() 
+    std::cout << "Parallel nested : " << (toc - tic).seconds()
               << " seconds" << std::endl;
 
 #endif
 
-    delete [] b;
+    delete[] b;
 
     return 0;
 }
--- a1/code/tbb-workspace/tasks/fib.cpp
+++ a1/code/tbb-workspace/tasks/fib.cpp
@@ -1,7 +1,7 @@
 /**
  * @file
  * Task execution demo program
- */ 
+ */
 #include <cstdlib>
 #include <iostream>
 
@@ -10,66 +10,64 @@
 #include "tbb/task_scheduler_init.h"
 #include "tbb/tick_count.h"
 
-long SerialFib(long n)
-{
-    if ( n < 2 )
+long SerialFib(long n) {
+    if (n < 2)
         return n;
     else
-        return SerialFib(n-1) + SerialFib (n-2);
+        return SerialFib(n - 1) + SerialFib(n - 2);
 }
 
 /**
  * Fibonacci task class
- */ 
-class FibTask: public tbb::task {
-    public:
-        const long n;
-        long * const sum;
+ */
+class FibTask : public tbb::task {
+public:
+    const long n;
+    long *const sum;
 
-        // constructor
-        FibTask(long n_, long *sum_): n(n_), sum(sum_) {}
+    // constructor
+    FibTask(long n_, long *sum_) : n(n_), sum(sum_) {}
 
-        // every task subclass must define the execute function
-        task* execute() {
+    // every task subclass must define the execute function
+    task *execute() {
 
-            if ( n < 16 ) {
-                *sum = SerialFib(n);
-            } else {
-                long x,y;
+        if (n < 16) {
+            *sum = SerialFib(n);
+        } else {
+            long x, y;
 
-                // allocate children tasks, a and b
-                FibTask& a = *new(allocate_child() ) FibTask(n-1,&x);
-                FibTask& b = *new(allocate_child() ) FibTask(n-2,&y);
-                set_ref_count(3); // set ref_count to #children + 1
+            // allocate children tasks, a and b
+            FibTask &a = *new (allocate_child()) FibTask(n - 1, &x);
+            FibTask &b = *new (allocate_child()) FibTask(n - 2, &y);
+            set_ref_count(3); // set ref_count to #children + 1
 
-                // Spawn b
-                tbb::task::spawn(b);
+            // Spawn b
+            tbb::task::spawn(b);
 
-                // Spawn a and wait for all children (a and b)
-                tbb::task::spawn_and_wait_for_all(a);
+            // Spawn a and wait for all children (a and b)
+            tbb::task::spawn_and_wait_for_all(a);
 
-                *sum = x+y;
-            }
-            return NULL;
+            *sum = x + y;
         }
+        return NULL;
+    }
 };
 
 /**
  * Driver function for task-based parallel implementation 
  * @param n Fibonacci number to compute
  * @return Fibonacci result
- */ 
-long ParallelFibTask(long n)
-{
+ */
+long ParallelFibTask(long n) {
     long sum;
 
     // create and spawn root task
-    FibTask& a = *new(tbb::task::allocate_root()) FibTask(n,&sum);
+    FibTask &a = *new (tbb::task::allocate_root()) FibTask(n, &sum);
     tbb::task::spawn_root_and_wait(a);
 
     return sum;
 }
- 
+
 #ifdef USE_LAMBDAS
 
 /**
@@ -77,62 +75,56 @@ long ParallelFibTask(long n)
  * and Lambda expressions.
  * @param n Fibonacci number to compute
  * @return Fibonacci result
- */ 
-long ParallelFibTG(long n) 
-{
-    if ( n < 16 )
+ */
+long ParallelFibTG(long n) {
+    if (n < 16)
         return SerialFib(n);
     else {
         int x, y;
         tbb::task_group g;
-        g.run( [&]{ x = ParallelFibTG(n-1);} );
-        g.run( [&]{ y = ParallelFibTG(n-2);} );
+        g.run([&] { x = ParallelFibTG(n - 1); });
+        g.run([&] { y = ParallelFibTG(n - 2); });
         g.wait();
-        return x+y;
+        return x + y;
     }
 }
 
 #endif
 
-
 /**
  * @param argv[1] Fibonacci number to compute
  * @param argv[2] number of threads
- */ 
-int main(int argc, char *argv[]) 
-{
+ */
+int main(int argc, char *argv[]) {
     long serial_result, parallel_result;
     const int input = atoi(argv[1]);
     const int nthreads = atoi(argv[2]);
 
-    tbb::tick_count tic, toc; 
-    tic = tbb::tick_count::now ();
+    tbb::tick_count tic, toc;
+    tic = tbb::tick_count::now();
     serial_result = SerialFib(input);
-    toc = tbb::tick_count::now ();
-    std::cerr << "Serial : " <<  (toc-tic).seconds () 
+    toc = tbb::tick_count::now();
+    std::cerr << "Serial : " << (toc - tic).seconds()
               << " seconds" << std::endl;
     std::cerr << "result: " << serial_result << std::endl;
-  
+
     tbb::task_scheduler_init init(nthreads);
 
-    tic = tbb::tick_count::now ();
+    tic = tbb::tick_count::now();
     parallel_result = ParallelFibTask(input);
-    toc = tbb::tick_count::now ();
-    std::cerr << "Parallel task : " <<  (toc-tic).seconds () 
+    toc = tbb::tick_count::now();
+    std::cerr << "Parallel task : " << (toc - tic).seconds()
               << " seconds" << std::endl;
     std::cerr << "result: " << parallel_result << std::endl;
 
-#ifdef USE_LAMBDAS 
-    tic = tbb::tick_count::now ();
+#ifdef USE_LAMBDAS
+    tic = tbb::tick_count::now();
     parallel_result = ParallelFibTG(input);
-    toc = tbb::tick_count::now ();
-    std::cerr << "Parallel task group : " <<  (toc-tic).seconds () 
+    toc = tbb::tick_count::now();
+    std::cerr << "Parallel task group : " << (toc - tic).seconds()
               << " seconds" << std::endl;
     std::cerr << "result: " << parallel_result << std::endl;
 #endif
-
-   
-
 
     return 0;
 }
