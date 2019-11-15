6ea446d65a74e2bde24fb1921cb3efc1dae9ce48
Alexis Zamanis <alexiszamanis1997@gmail.com>
2019-11-15 20:46:35 +0200

    minor fix

--- lab1/ask2/openmp/run.sh
+++ lab1/ask2/openmp/run.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 #PBS -N run-fw_tiled-4096
-#PBS -o run-fw_tiled-4096
-#PBS -e run-fw_tiled-4096
+#PBS -o run-fw_tiled-4096.out
+#PBS -e run-fw_tiled-4096.err
 #PBS -l nodes=sandman:ppn=64
 #PBS -l walltime=00:10:00
 
 module load openmp
 cd /home/parallel/parlab02/pps/lab1/ask2/openmp
+export OMP_DISPLAY_ENV=true
+export OMP_SCHEDULE=static
+export OMP_PROC_BIND=true
 for OMP_NUM_THREADS in 1 2 4 8 16 32 64
 do
     export OMP_NUM_THREADS=$OMP_NUM_THREADS
--- lab1/ask2/openmp/submit.sh
+++ lab1/ask2/openmp/submit.sh
@@ -5,8 +5,8 @@ do
     for B in 1 2 4 8 16 32 64 128 256 512 1024 2048 4096
     do
         sed -i "s/N run.*/N run-$fw-$B/" run.sh
-        sed -i "s/o run.*/o run-$fw-$B/" run.sh
-        sed -i "s/e run.*/e run-$fw-$B/" run.sh
+        sed -i "s/o run.*/o run-$fw-$B.out/" run.sh
+        sed -i "s/e run.*/e run-$fw-$B.err/" run.sh
         sed -i "s/\.\/.*/.\/$fw \$N $B/" run.sh
         cat run.sh
         # qsub -q serial run.sh
