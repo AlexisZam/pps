ab7b20a88232aa67dce6d931417388db125489d7
Alexis Zamanis <alexiszamanis1997@gmail.com>
2019-11-19 04:05:24 +0200

    run done

--- lab1/ask2/openmp/run.sh
+++ lab1/ask2/openmp/run.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-#PBS -N run-fw_sr-4096
-#PBS -o run-fw_sr-4096.out
-#PBS -e run-fw_sr-4096.err
+#PBS -N run
+#PBS -o run.out
+#PBS -e run.err
 #PBS -l nodes=sandman:ppn=64
 #PBS -l walltime=00:10:00
 
 module load openmp
 cd /home/parallel/parlab02/pps/lab1/ask2/openmp
-export OMP_DISPLAY_ENV=true
-export OMP_SCHEDULE=static
 export OMP_PROC_BIND=true
 for OMP_NUM_THREADS in 1 2 4 8 16 32 64
 do
@@ -17,6 +15,6 @@ do
     echo Number of threads: $OMP_NUM_THREADS
     for N in 1024 2048 4096
     do
-        ./fw_sr $N 4096
+        ./fw_tiled $N 64
     done
 done
