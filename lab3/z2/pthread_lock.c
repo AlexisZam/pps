7b85b36350191d7b194fca4e86c6407222572807
Alexis Zamanis <alexiszamanis1997@gmail.com>
2019-12-21 17:14:44 +0200

    added lab3

---
+++ lab3/z2/pthread_lock.c
@@ -0,0 +1,25 @@
+#include "../common/alloc.h"
+#include "lock.h"
+
+struct lock_struct {
+    /* Delete this in your implementation, just a placeholder. */
+    int dummy;
+};
+
+lock_t *lock_init(int nthreads) {
+    lock_t *lock;
+
+    XMALLOC(lock, 1);
+    /* other initializations here. */
+    return lock;
+}
+
+void lock_free(lock_t *lock) {
+    XFREE(lock);
+}
+
+void lock_acquire(lock_t *lock) {
+}
+
+void lock_release(lock_t *lock) {
+}
