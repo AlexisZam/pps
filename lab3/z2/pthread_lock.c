7c3ae6bf8c2bee6761a6154e275fbe2312364b2b
Alexis Zamanis <alexiszamanis1997@gmail.com>
2020-01-17 04:18:24 +0200

    minor change

--- lab3/z2/pthread_lock.c
+++ lab3/z2/pthread_lock.c
@@ -4,26 +4,26 @@
 #include "lock.h"
 
 struct lock_struct {
-    pthread_spinlock_t lock;
+    pthread_spinlock_t state;
 };
 
 lock_t *lock_init(int nthreads) {
     lock_t *lock;
 
     XMALLOC(lock, 1);
-    pthread_spin_init(&lock->lock, PTHREAD_PROCESS_PRIVATE);
+    pthread_spin_init(&lock->state, PTHREAD_PROCESS_PRIVATE);
     return lock;
 }
 
 void lock_free(lock_t *lock) {
-    pthread_spin_destroy(&lock->lock);
+    pthread_spin_destroy(&lock->state);
     XFREE(lock);
 }
 
 void lock_acquire(lock_t *lock) {
-    pthread_spin_lock(&lock->lock);
+    pthread_spin_lock(&lock->state);
 }
 
 void lock_release(lock_t *lock) {
-    pthread_spin_unlock(&lock->lock);
+    pthread_spin_unlock(&lock->state);
 }
