bd0de8018dfa3cdf47422545cbbf1ddb634f5ceb
Alexis Zamanis <alexiszamanis1997@gmail.com>
2020-03-02 17:04:24 +0200

    major changes

--- lab4/dmm-skeleton/cuda/dmm_gpu.cu
+++ lab4/dmm-skeleton/cuda/dmm_gpu.cu
@@ -16,15 +16,15 @@ __global__ void dmm_gpu_naive(const value_t *A, const value_t *B, value_t *C,
     /*
    * FILLME: fill the code.
    */
-    int i = blockIdx.y * blockDim.y + threadIdx.y;
-    int j = blockIdx.x * blockDim.x + threadIdx.x;
+    for (int i = blockIdx.y * blockDim.y + threadIdx.y; i < M; i += blockDim.y * gridDim.y)
+        for (int j = blockIdx.x * blockDim.x + threadIdx.x; j < N; j += blockDim.x * gridDim.x) {
+            value_t _Cij = 0;
 
-    value_t _Cij = 0;
+            for (int k = 0; k < K; ++k)
+                _Cij += A[i * K + k] * B[k * N + j];
 
-    for (int k = 0; k < K; ++k)
-        _Cij += A[i * K + k] * B[k * N + j];
-
-    C[i * N + j] = _Cij;
+            C[i * N + j] = _Cij;
+        }
 }
 
 /*
@@ -36,26 +36,26 @@ __global__ void dmm_gpu_coalesced_A(const value_t *A, const value_t *B,
     /*
    * FILLME: fill the code.
    */
-    int i = blockIdx.y * blockDim.y + threadIdx.y;
-    int j = blockIdx.x * blockDim.x + threadIdx.x;
+    for (int i = blockIdx.y * blockDim.y + threadIdx.y; i < M; i += blockDim.y * gridDim.y)
+        for (int j = blockIdx.x * blockDim.x + threadIdx.x; j < N; j += blockDim.x * gridDim.x) {
+            value_t _Cij = 0;
 
-    value_t _Cij = 0;
+            for (int m = 0; m < K; m += TILE_X) {
+                __shared__ value_t As[THREAD_BLOCK_Y][TILE_X];
 
-    for (int m = 0; m < K; m += TILE_X) {
-        __shared__ value_t As[THREAD_BLOCK_Y][TILE_X];
+                for (int n = threadIdx.x; n < TILE_X; n += blockDim.x)
+                    As[threadIdx.y][n] = A[i * K + n + m];
 
-        for (int n = threadIdx.x; n < TILE_X; n += blockDim.x)
-            As[threadIdx.y][n] = A[i * K + n + m];
+                __syncthreads();
 
-        __syncthreads();
+                for (int k = 0; k < TILE_X; ++k)
+                    _Cij += As[threadIdx.y][k] * B[(k + m) * N + j];
 
-        for (int k = 0; k < TILE_X; ++k)
-            _Cij += As[threadIdx.y][k] * B[(k + m) * N + j];
+                __syncthreads();
+            }
 
-        __syncthreads();
-    }
-
-    C[i * N + j] = _Cij;
+            C[i * N + j] = _Cij;
+        }
 }
 
 /*
@@ -66,28 +66,29 @@ __global__ void dmm_gpu_reduced_global(const value_t *A, const value_t *B, value
     /*
    * FILLME: fill the code.
    */
-    int i = blockIdx.y * blockDim.y + threadIdx.y;
-    int j = blockIdx.x * blockDim.x + threadIdx.x;
+    for (int i = blockIdx.y * blockDim.y + threadIdx.y; i < M; i += blockDim.y * gridDim.y)
+        for (int j = blockIdx.x * blockDim.x + threadIdx.x; j < N; j += blockDim.x * gridDim.x) {
+            value_t _Cij = 0;
 
-    value_t _Cij = 0;
-    for (int m = 0; m < K; m += TILE_X) {
-        __shared__ value_t As[THREAD_BLOCK_Y][TILE_X];
-        __shared__ value_t Bs[TILE_X][THREAD_BLOCK_X];
+            for (int m = 0; m < K; m += TILE_X) {
+                __shared__ value_t As[THREAD_BLOCK_Y][TILE_X];
+                __shared__ value_t Bs[TILE_X][THREAD_BLOCK_X];
 
-        for (int n = threadIdx.x; n < TILE_X; n += blockDim.x)
-            As[threadIdx.y][n] = A[i * K + n + m];
-        for (int n = threadIdx.y; n < TILE_X; n += blockDim.y)
-            Bs[n][threadIdx.x] = B[(n + m) * N + j];
+                for (int n = threadIdx.x; n < TILE_X; n += blockDim.x)
+                    As[threadIdx.y][n] = A[i * K + n + m];
+                for (int n = threadIdx.y; n < TILE_X; n += blockDim.y)
+                    Bs[n][threadIdx.x] = B[(n + m) * N + j];
 
-        __syncthreads();
+                __syncthreads();
 
-        for (int k = 0; k < TILE_X; ++k)
-            _Cij += As[threadIdx.y][k] * Bs[k][threadIdx.x];
+                for (int k = 0; k < TILE_X; ++k)
+                    _Cij += As[threadIdx.y][k] * Bs[k][threadIdx.x];
 
-        __syncthreads();
-    }
+                __syncthreads();
+            }
 
-    C[i * N + j] = _Cij;
+            C[i * N + j] = _Cij;
+        }
 }
 
 /*
